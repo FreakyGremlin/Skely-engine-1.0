05d9d04e7487368d3298e5b4ad07cebb1926c066
LukiFriml <lukas.p.friml@gmail.com>
2025-10-24 08:37:03 +1300

    start menju

---
+++ start_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+
+func _on_start_pressed():
+	get_tree().change_scene_to_file( "res://Scenes/Main.tscn")
+
+func _on_options_pressed():
+	print("Options pressed")
+
+
+func _on_exit_pressed():
+	get_tree().quit()
